e43a3b7d9a2cfb5f95abe45603a7143f7031dbdd
hrmussa <andersgiovanni@gmail.com>
2022-04-26 22:15:38 +0200

    [WIP] mIOU

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Image segmentation with deep encoder-decoder networks using the Cambridge-drivin
 ## The Data
 
 CamVid (Cambridge-driving Labeled Video Database) is a road/driving scene understanding database which was originally captured as five video sequences with a 960×720 resolution camera mounted on the dashboard of a car. Those sequences were sampled (four of them at 1 fps and one at 15 fps) adding up to 701 frames. Those stills were manually annotated with 32 classes: void, building, wall, tree, vegetation, fence, sidewalk, parking block, column/pole, traffic cone, bridge, sign, miscellaneous text, traffic light, sky, tunnel, archway, road, road shoulder, lane markings (driving), lane markings (non-driving), animal, pedestrian, child, cart luggage, bicyclist, motorcycle, car, SUV/pickup/truck, truck/bus, train, and other moving object. 
+
+## Install environment and packages
+
+- Create new virtual environment: `python -m venv .venv`
+- Activate environment: `source .venv/bin/activate`
+- Install packages from file: `pip install -r requirements.txt`
+
--- src/AEModel.py
+++ src/AEModel.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms.functional as TF
+from sklearn.metrics import jaccard_score
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms as T
@@ -17,7 +18,33 @@ class AEModelTrainer:
         self.model: nn.Module = model
 
     def train(self, train_loader: DataLoader, val_loader: DataLoader, epochs: int, optimizer: optim.Optimizer, loss_fn: Callable, scaler: torch.cuda.amp.GradScaler, log_name: str) -> Tuple[List[float], List[float]]:
-        
+        """
+        Train the model for a given number of epochs.
+
+        Parameters
+        ----------
+        train_loader : DataLoader
+            Dataloader with training data
+        val_loader : DataLoader
+            Dataloader with validation data
+        epochs : int
+            Number of epochs to train for
+        optimizer : optim.Optimizer
+            Optimizer to use for training
+        loss_fn : Callable
+            Loss function to use for training
+        scaler : torch.cuda.amp.GradScaler
+            Gradient scaler to use for training
+        log_name : str
+            Name of the log file to use
+
+        Returns
+        -------
+        Tuple[List[float], List[float]]
+            Training and validation losses
+        """
+
+
         # Summary writer for Tensorboard
         writer = SummaryWriter(log_dir='runs/' + log_name)
 
@@ -77,18 +104,30 @@ class AEModelTrainer:
             # Calculate average loss
             train_loss /= len(train_loader)
 
+            # save jaccard score
+            jaccard_scores = list()
+
             # Calculate validation loss
             self.model.eval()
             with torch.no_grad():
                 for batch_idx, (data, targets) in enumerate(val_loader):
+                    # move data to device
                     data = data.to(device)
                     targets = targets.float().to(device)
+                    # forward
                     predictions = self.model(data)
+                    # calculate loss
                     loss = loss_fn(predictions, targets)
+                    # add batch loss to total loss
                     valid_loss += loss.item()
+                    # calculate jaccard score
+                    jaccard_scores.append(jaccard_score(targets.cpu().numpy(), predictions.cpu().numpy()))
             
             # Calculate average loss
             valid_loss /= len(val_loader)
+
+            # average jaccard score mIOU
+            jaccard_score = sum(jaccard_scores) / len(jaccard_scores)
 
             stop = time()
 
@@ -99,7 +138,57 @@ class AEModelTrainer:
             train_losses.append(train_loss), valid_losses.append(valid_loss)
             writer.add_scalar('Loss/train', train_loss, epoch)
             writer.add_scalar('Loss/val', valid_loss, epoch)
+            writer.add_scalar('Jaccard/val', jaccard_score, epoch)
 
         writer.close()
 
         return train_losses, valid_losses
+
+    def predict(self, test_loader: DataLoader) -> List[Tuple[torch.Tensor, torch.Tensor, List[float]]]:
+        """
+        Predict the labels for the test set and compute the jaccard score.
+
+        Parameters
+        ----------
+        test_loader : DataLoader
+            Dataloader with test data
+
+        Returns
+        -------
+        List[Tuple[torch.Tensor, torch.Tensor, List[float]]]
+            List of tuples containing predictions, targets and jaccard scores
+        """
+
+
+        # Get device
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        print(f'Using {device} as backend')
+        
+        # activate evaluation mode
+        self.model.eval()
+
+
+        with torch.no_grad():
+            # Create empty list for predictions and targets
+            predictions = list()
+            targets = list()
+
+            # Iterate over batches
+            for batch_idx, (data, target) in enumerate(test_loader):
+                
+                # move data to device
+                data = data.to(device)
+                target = target.float().to(device)
+
+                with torch.cuda.amp.autocast():
+                    # forward
+                    prediction = self.model(data)
+                    predictions.append(prediction)
+                    targets.append(target)
+
+        # calculate jaccard score
+        jaccard_scores = list()
+        for prediction, target in zip(predictions, targets):
+            jaccard_scores.append(jaccard_score(target.cpu().numpy(), prediction.cpu().numpy()))
+        
+        return predictions, targets, jaccard_scores
