096704ab37290d706c9ef47443dcc01acfe410fe
hrmussa <andersgiovanni@gmail.com>
2022-05-09 14:52:15 +0200

    add info to runners

--- runner_vgg.sh
+++ runner_vgg.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=vgg    # Job name
+#SBATCH --output=outputs/vgg.%j.out      # Name of output file (%j expands to jobId)
+#SBATCH --cpus-per-task=16       # Schedule one core
+#SBATCH --gres=gpu               # Schedule a GPU
+#SBATCH --time=08:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --partition=brown,red    # Run on either the Red or Brown queue
+#SBATCH --mail-type=FAIL,END    # Send an email when the job finishes or fails
+
 echo "No pretraining"
 
 python src/VGG11.py --augmentation none --pretraining False
--- runner_vgg_p.sh
+++ runner_vgg_p.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=vgg_p    # Job name
+#SBATCH --output=outputs/vgg_p.%j.out      # Name of output file (%j expands to jobId)
+#SBATCH --cpus-per-task=16       # Schedule one core
+#SBATCH --gres=gpu               # Schedule a GPU
+#SBATCH --time=08:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --partition=brown,red    # Run on either the Red or Brown queue
+#SBATCH --mail-type=FAIL,END    # Send an email when the job finishes or fails
+
+
 echo "Pretraining"
 
 python src/VGG11.py --augmentation none --pretraining True
